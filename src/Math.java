c4fd0c65e8ca20c6a8e4650e6a255938866cb588
anb16 <alinenbrito@hotmail.com>
2020-02-01 20:09:57 -0300

    Updating min(x,y) method.

--- src/Math.java
+++ src/Math.java
@@ -2,7 +2,9 @@
 public class Math{
 	
 	public static double min(double x, double y){
-		return (x < y ? x : y);
+		double min = x < y ? x : y;
+		System.out.printf("The lowest value is %.2f", min);
+		return min;
 	}
 
 }
